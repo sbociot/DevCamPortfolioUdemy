8b4f7f1dbac101595a71665a11a2b93b5d3d092b
Steve Bocicot <stevebocicot@Steves-MacBook-Pro.local>
2018-01-08 00:54:36 -0500

    "portyfolio added"

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,52 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+    Blog.create!(
+        title: "My Blog Post #{blog}",
+        body:"Lorem ipsum dolor sit
+        amet, consectetur adipiscing
+        elit. Nulla vel orci vel
+        lectus varius molestie at
+        tincidunt ipsum. Etiam i
+        culis, mauris bibendum
+        consectetur rhoncus,
+        rat diam interdum libero,
+         at varius risus ante eu
+         ipsum. Morbi tempus lacus
+         eget augue auctor, eget
+         gravida dui dignissim. Maecenas
+          sem purus, dapibus ac neque et, t
+          incidunt dignissim felis. Integer
+           malesuada ex felis, et dignissim "
+    )
+end
+puts "10 blogs pots created"
+
+5.times do |skill|
+    Skill.create!(
+        title: "Rails #{skill}",
+        percent_utilized:"15"
+    )
+end
+
+9.times do |portfolio_item|
+    Portfolio.create!(
+        title:"Portfolio item: #{portfolio_item}",
+        subtitle:"My great service",
+        body:"Lorem ipsum dolor sit
+        amet, consectetur adipiscing
+        elit. Nulla vel orci vel
+        lectus varius molestie at
+        tincidunt ipsum. Etiam i
+        culis, mauris bibendum
+        consectetur rhoncus,
+        rat diam interdum libero,
+         at varius risus ante eu
+         ipsum. Morbi tempus lacus
+         eget augue auctor, eget
+         gravida dui dignissim. Maecenas
+          sem purus, dapibus ac neque et, t
+          incidunt dignissim felis. Integer
+           malesuada ex felis, et dignissim ",
+        main_image:"http://placeholder/600*400",
+        thum_image:"http://placeholder/350*200"
+    )
+end
